e53c300712ac142d18dc429f82055a33caf4adc7
Markel Iturbe Rojano <markel_iturbe@euneiz.com>
2026-04-30 15:36:34 +0200

    fix: remove use_oidc from backend block, handled via env vars

--- soc-terraform/main.tf
+++ soc-terraform/main.tf
@@ -7,15 +7,13 @@ terraform {
   }
 
   backend "azurerm" {
-    use_oidc             = true
-    use_azuread_auth     = true
-    tenant_id            = "78f3a279-48c8-4670-9162-a63c451c9fae"
-    client_id            = "99dd29f9-ea6c-48b0-afcc-6f4e2e10d375"
-    storage_account_name = "tfstatesocml"
-    container_name       = "tfstate"
-    key                  = "terraform.tfstate"
-    resource_group_name  = "rg-soc-proyecto"
-  }
+  tenant_id            = "78f3a279-48c8-4670-9162-a63c451c9fae"
+  client_id            = "99dd29f9-ea6c-48b0-afcc-6f4e2e10d375"
+  storage_account_name = "tfstatesocml"
+  container_name       = "tfstate"
+  key                  = "terraform.tfstate"
+  resource_group_name  = "rg-soc-proyecto"
+ }
 }
 
 provider "azurerm" {
